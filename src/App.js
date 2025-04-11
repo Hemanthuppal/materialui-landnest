6ee3bbe27b878468f0e2753656f46824ed031cd2
yanamadalaraju <150237963+yanamadalaraju@users.noreply.github.com>
2025-04-11 20:39:13 +0530

    added rajesh folder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Dashboard from './bharath/Dashboard';
 import Details_Page from './sharvani/Details_Page';
 import Vendor_Registration from './maniteja/Vendor_Registration';
 import Property_Map from './Sritajana/Property_Map';
+import Lease_map from './Rajesh/Lease_map';
+import Lease_detail from './Rajesh/Lease_details';
+import MobilePropertyUI from './tharun/MobileProperyUI';
+import InteriorServices from './bharath/Interior_service';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
         <Route path="/details" element={<Details_Page />} />
         <Route path="/vendor" element={<Vendor_Registration />} />
         <Route path="/propertymap" element={<Property_Map />} />
+        <Route path="/lease_map" element={<Lease_map />} />
+        <Route path="/lease_detail" element={<Lease_detail />} />
+        <Route path="/mobileproperty" element={<MobilePropertyUI />} />
+        <Route path="/interior_service" element={<InteriorServices />} />
       </Routes>
     </BrowserRouter>
   );
