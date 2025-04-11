d3db3295f36894479b38d78992d8f394df942276
unknown <sharvanikokkonda@gmail.com>
2025-04-11 20:05:39 +0530

    added rent form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './bharath/Dashboard';
 import Details_Page from './sharvani/Details_Page';
 import Vendor_Registration from './maniteja/Vendor_Registration';
 import Property_Map from './Sritajana/Property_Map';
+import RentForm from './maniteja/Rent_form';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/details" element={<Details_Page />} />
         <Route path="/vendor" element={<Vendor_Registration />} />
         <Route path="/propertymap" element={<Property_Map />} />
+        <Route path="/rent" element={<RentForm />} />
       </Routes>
     </BrowserRouter>
   );
