1231dc417c53cfd3f1ee68bd6bf50ec995673c87
srithaalwar <srithaalwar4@gmail.com>
2025-04-12 11:28:33 +0530

    sritha

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './bharath/Dashboard';
 import Details_Page from './sharvani/Details_Page';
 import Vendor_Registration from './maniteja/Vendor_Registration';
 import Property_Map from './Sritajana/Property_Map';
+import Saved_Buy from './Sritajana/Savedbuy';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/details" element={<Details_Page />} />
         <Route path="/vendor" element={<Vendor_Registration />} />
         <Route path="/propertymap" element={<Property_Map />} />
+        <Route path="/Savedbuy" element={<Saved_Buy />} />
       </Routes>
     </BrowserRouter>
   );
