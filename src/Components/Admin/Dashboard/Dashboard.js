c1abda36430fd2c756219098ed6e84e08036900e
bharathkusuma123 <bharathkusuma4953@gmail.com>
2025-04-19 15:42:33 +0530

    bharath

--- src/Components/Admin/Dashboard/Dashboard.js
+++ src/Components/Admin/Dashboard/Dashboard.js
@@ -1,11 +1,22 @@
-import React from 'react'
+// src/bharath/Dashboard.js
+import React from 'react';
+import AdminLayout from '../../Admin/LayOut/AdminLayout';
+
+const DashboardContent = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Dashboard Content</h1>
+      {/* Your dashboard content here */}
+    </div>
+  );
+};
 
 const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <AdminLayout>
+      <DashboardContent />
+    </AdminLayout>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
