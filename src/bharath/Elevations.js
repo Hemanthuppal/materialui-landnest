ddd4c387c7af67017cf3ff2daf36a075007612a6
bharathkusuma123 <bharathkusuma4953@gmail.com>
2025-04-21 16:10:51 +0530

    bharath

--- src/bharath/Elevations.js
+++ src/bharath/Elevations.js
@@ -89,7 +89,7 @@ const Elevations = () => {
               navigate('/home-service');
               break;
             case 'profile':
-              navigate('/profile');
+              navigate('/work-detail');
               break;
             default:
               navigate('/');
